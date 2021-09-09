24dc47c6fc6136b935823a42835e80bf59897e77
MarcelaLeon <marcelatorresleon15@gmail.com>
2021-09-09 16:14:21 -0500

    [UPDATE] adicion de etiquetas ul

--- src/components/Eleccion/index.jsx
+++ src/components/Eleccion/index.jsx
@@ -8,7 +8,9 @@ export class Eleccion extends React.Component{
                 <h3>Seleccion anterior: {this.props.anterior}</h3>
 
                 <h3>Historial de opciones elegidas:</h3>
+                <ul>
                 {this.props.selecciones.map((ele, i )=> <li key={i}>{ele}</li>)}
+                </ul>
             </div>
         )
     }
